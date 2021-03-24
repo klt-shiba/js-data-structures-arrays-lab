59d27775c9f4448bbceafc71868855ef2cf1295b
Colin Tseng <klt.aus@yahoo.com>
2021-03-24 21:39:01 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(a) {
+    drivers.push(a); 
+}
+function destructivelyPrependDriver(a) {
+    drivers.unshift(a); 
+}
+function destructivelyRemoveLastDriver(a) {
+    drivers.pop(a); 
+}
+function destructivelyRemoveFirstDriver(a) {
+    drivers.shift(a); 
+}
+function appendDriver(a) {
+    let b = [...drivers]
+    b.push(a); 
+    return b
+}
+function prependDriver(a) {
+    let b = [...drivers]
+    b.unshift(a); 
+    return b
+}
+function removeLastDriver(a) {
+    let b = [...drivers]
+    b.pop(a); 
+    return b
+}
+function removeFirstDriver(a) {
+    let b = [...drivers]
+    b.shift(a); 
+    return b
+}
+
